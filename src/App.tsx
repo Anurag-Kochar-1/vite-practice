c634f547c1821a14755ca11be3ec20d43712622c
Anurag Kochar <93912807+Anurag-Kochar-1@users.noreply.github.com>
2022-12-11 17:34:53 +0000

    first

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import './App.css'
 
 export default function App() {
   return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit 🌀
+    <main className='w-full h-screen bg-gray-200'>
+      <h1 className="text-green-400 text-3xl"> Hello replit </h1>
+      <h2 className="text-green-300 text-lg"> Hey </h2>
     </main>
   )
 }
