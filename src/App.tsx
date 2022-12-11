27f972658a0bcb8aebe1ef7156ba0766b30f2455
Anurag Kochar <93912807+Anurag-Kochar-1@users.noreply.github.com>
2022-12-11 17:56:54 +0000

    alias-done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import './App.css'
 
+
+type UserType = {
+  name: string
+  age: number
+  gender?: string
+}
+
 export default function App() {
+
+  const firstUser: UserType = {
+    name: "Anurag",
+    age: 19,
+    gender: "male"
+  }
+
+  const login = (userData: UserType): UserType => {
+    return userData
+  }
+
+
+
+
   return (
-    <main className='w-full h-screen bg-gray-200'>
-      <h1 className="text-green-400 text-3xl"> Hello replit </h1>
-      <h2 className="text-green-300 text-lg"> Hey </h2>
+    <main className='w-full h-screen bg-purple-200'>
+      <h1 className="text-blue-100 text-normal bg-blue-400 rounded-full px-4 py-1 inline-block"
+        onClick={() => console.log(login( firstUser ))}
+      > login </h1>
+
+
     </main>
   )
 }
